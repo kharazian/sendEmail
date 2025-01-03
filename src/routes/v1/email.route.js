1a29b4da6a7922f5f3c7de9882ff8655f4f03bd7
Kharazian <AmirKharazian@gmail.com>
2025-01-03 16:35:30 -0500

    add error handler for send email

--- src/routes/v1/email.route.js
+++ src/routes/v1/email.route.js
@@ -7,8 +7,14 @@ const router = express.Router();
 router
   .route('/')
   .post(auth(), (req, res) => {
-    emailService.sendAppointment(req.body);
-    res.send('Create a user');
+    emailService.sendAppointment(req.body)
+    .then(() => {
+      res.send('Appointment email sent'); 
+    })
+    .catch(error => {
+      console.error('Error sending appointment email:', error);
+      res.status(500).send('Error sending appointment email'); 
+    });
   });
 
   module.exports = router;
